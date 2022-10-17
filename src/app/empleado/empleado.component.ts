e1eb0e63e4b9979dd582c946e3141125eefadc03
AaronPecesG <aaron.peces@alu.uclm.es>
2022-10-17 12:13:15 +0200

    Event Binding + Interpolacion + Property Binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -23,9 +23,19 @@ export class EmpleadoComponent implements OnInit {
 
   habilitacionCuadro=false;
   usuRegistrado=false;
-
+  textoDeRegistro = "No hay nadie registrado"
   getRegistroUsuario(){
-    this.usuRegistrado=true;
+    this.usuRegistrado=false;
+  }
+
+  setUsuarioRegistrado(event:Event){
+    //alert("El usuario se acaba de registrar");
+    //this.textoDeRegistro ="El usuario se ha registrado"
+    if((<HTMLInputElement>event.target).value == "si"){
+      this.textoDeRegistro="El usuario se acaba de registrar";
+    }else{
+      this.textoDeRegistro="No hay nadie registrado"
+    }
   }
 
   constructor() { }
