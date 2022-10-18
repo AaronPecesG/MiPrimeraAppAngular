a7e4e5596025e761366eafd35fa62cdcd4e85883
AaronPecesG <aaron.peces@alu.uclm.es>
2022-10-18 13:08:18 +0200

    Directivas 1

--- trabajoDirectivaApp/src/app/app.component.ts
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de usuarios';
+  nombre:string = "";
+  apellido:string=""
+  mensaje = '';
+  registrado = false;
+  entradas ;
+
+
+  constructor() {
+    this.entradas=[
+      {titulo:"Python cada día más presente"},
+      {titulo:"Java presente desde hace mñas de 20 años"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"JavaScript cada vez mas funcional"},
+      {titulo:"¿Dónde quedó Pascal?"},
+    ]
+  }
+
+  registrarUsuario(){
+    this.registrado=true;
+    this.mensaje="Usuario registrado correctamente"
+  }
 }
