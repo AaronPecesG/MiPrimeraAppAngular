883a8f7683bb8659bffe116c9eed862917e0db0f
AaronPecesG <aaron.peces@alu.uclm.es>
2022-10-19 17:12:18 +0200

    Servicios III, servicio dentro de otro servicio

--- appEmpleados/src/app/empleados.service.ts
+++ appEmpleados/src/app/empleados.service.ts
@@ -1,6 +1,12 @@
 import {Empleado} from "./empleado.model";
+import {Injectable} from "@angular/core";
+import {ServicioEmpleadosService} from "./servicio-empleados.service";
 
+@Injectable()
 export class EmpleadosService{
+  constructor(private servicioVentanaEmergente:ServicioEmpleadosService) {
+
+  }
   empleados:Empleado[]=[
     new Empleado("Aaron","Peces","Presidente",10000),
     new Empleado("Iria","Soler","Directora",7500),
@@ -9,6 +15,7 @@ export class EmpleadosService{
   ];
 
   agregarEmpleadoServicio(empleado:Empleado){
+    this.servicioVentanaEmergente.muestraMensaje("Persona que se va a agregar: "+"\n"+empleado.nombre+"\n"+"Salario: "+empleado.salario);
     this.empleados.push(empleado);
   }
 
