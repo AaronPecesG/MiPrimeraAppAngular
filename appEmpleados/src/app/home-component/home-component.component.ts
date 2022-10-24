49d83c1fdba02d64b131e1c4699a5f6225a6b3fe
AaronPecesG <aaron.peces@alu.uclm.es>
2022-10-24 11:02:57 +0200

    Routing II. Adaptando antigua aplicación a routing

--- appEmpleados/src/app/home-component/home-component.component.ts
+++ appEmpleados/src/app/home-component/home-component.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {Empleado} from "../empleado.model";
+import {ServicioEmpleadosService} from "../servicio-empleados.service";
+import {EmpleadosService} from "../empleados.service";
+
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.css']
 })
-export class HomeComponentComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponentComponent implements OnInit {
+  titulo = 'Listado de empleados';
+
+  empleados:Empleado[]=[];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+
+  constructor(private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService) {
+    //this.empleados=this.empleadosService.empleados;
+  }
 
   ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados;
+  }
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    //this.miServicio.muestraMensaje("Nombre del empleado: " +miEmpleado.nombre);
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
   }
 
 }
